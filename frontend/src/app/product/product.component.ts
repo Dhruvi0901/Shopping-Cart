bf28b14476de6ed2689c1dfa956c99d0badb29c8
dhruvi <dhruvi.219beceg005@gmail.com>
2022-02-23 15:01:37 +0530

    commit

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -11,45 +11,57 @@ import { __param } from 'tslib';
 })
 export class ProductComponent implements OnInit {
 
-product : Product = new Product();
- public profileId : Number | undefined;
- public productId!: Number;
+  product: Product = new Product();
+  public profileId: Number | undefined;
+  public productId!: Number;
+  public updateBtn = "none";
+  public saveBtn = "inline";
 
 
   constructor(private productService: ProductserviceService, private route: ActivatedRoute, private router: Router) {
 
-    this.route.queryParams.subscribe(params=>{
+    this.route.queryParams.subscribe(params => {
       this.profileId = Number(params['admin'])
     })
 
-    this.route.queryParams.subscribe(params=>{
+    this.route.queryParams.subscribe(params => {
       this.productId = Number(params['editId'])
     })
-   }
-
+  }
   ngOnInit(): void {
- 
-    // if(this.productId >= 1){
-      
+    if (this.productId >= 1) {
       this.fillData()
-    // }
+    }
   }
 
-  saveData(){
-     this.product.profileId = this.profileId 
-    this.productService.postProduct(this.product).subscribe((res)=>{
+  saveData() {
+    this.product.profileId = this.profileId
+    this.productService.postProduct(this.product).subscribe((res) => {
       console.log(res)
-      this.router.navigate(['/admin/'+this.profileId])
+      this.router.navigate(['/admin/' + this.profileId])
       console.log(this.profileId);
 
     })
   }
-  fillData(){
-    this.productService.getEditProduct(this.productId).subscribe((res)=>{
-      this.product.productName = 'Dhruvi'
+  fillData() {
+    this.productService.getEditProduct(this.productId).subscribe((res) => {
+      //this.product 
       this.product = res as Product;
-      console.log(this.product)
+
+      this.saveBtn = "none";
+      this.updateBtn = "inline";
+
     })
+  }
+
+  updateData(){
+
+    this.productService.putEditProduct(this.productId, this.product).subscribe((res)=>{
+      console.log(res)
+    })
+
+
+
   }
 
 }
